22aa19c2bb36facb627a77705c9d5d6ca5e0d5fc
AntonioIonica <antonioionica@gmail.com>
2021-11-01 12:50:21 +0200

    Update

--- src/Curs_9/main.py
+++ src/Curs_9/main.py
@@ -1,9 +1,8 @@
 """
 Main execution
 """
+from src.Curs_9.src.test_script import test_case1
 from src.Curs_9.src.utils import Utils
-from src.Curs_9.src.test_script import TestCases
-
 
 
 if __name__ == '__main__':
@@ -16,9 +15,7 @@ if __name__ == '__main__':
     utils_data = Utils()
     datas = utils_data.get_input_data('C:/Users/anton/PycharmProjects/Automation_testing/'
                                             'src/Curs_9/test_data/input_data.json')
-    cases = TestCases()
-    case_1 = cases.test_case1()
-    case_2 = cases.test_case2()
-    output_1 = utils_data.run_test(datas, case_1)
-    output_2 = utils_data.run_test(datas, case_2)
+    output = utils_data.run_test(datas, test_case1)
+    print(output)
+    utils_data.gen_report(output)
 
--- src/Curs_9/src/test_script.py
+++ src/Curs_9/src/test_script.py
@@ -2,33 +2,26 @@
 Test case-fuzzywuzzy
 """
 from fuzzywuzzy import fuzz
-from src.Curs_9.src.utils import Utils
 
 
-class TestCases(Utils):
+def test_case1(input_data, expected_data):
+    similarity = fuzz.partial_ratio(input_data, expected_data)
+    if similarity > 90:
+        print(f'Test passed with the score of {similarity}!')
+    elif 80 < similarity < 90:
+        print(f'Test passed with the score of {similarity}! There is a warning!')
+    else:
+        print(f'Test failed with the score of {similarity}!')
+    return similarity
 
-    def test_case1(self, input_data, expected_data):
-        similarity = fuzz.partial_ratio(input_data, expected_data)
-        if similarity > 90:
-            print(f'Test passed with the score of {similarity}!')
-        elif 80 < similarity < 90:
-            print(f'Test passed with the score of {similarity}! There is a warning!')
-        else:
-            print(f'Test failed with the score of {similarity}!')
-        return similarity
-
-    def test_case2(self, input_data, expected_data):
-        q_ratio = fuzz.QRatio(input_data, expected_data)
-        if q_ratio > 80:
-            print(f'Test passed with the score of QRatio of {q_ratio}!')
-        elif 60 < q_ratio < 80:
-            print(f'QRatio test passed with a score of {q_ratio}!')
-        else:
-            print(f'Test failed with a QRatio of {q_ratio}!')
-        return q_ratio
-
-cases = TestCases()
-case_1 = cases.test_case1()
-case_2 = cases.test_case2()
+def test_case2(input_data, expected_data):
+    q_ratio = fuzz.QRatio(input_data, expected_data)
+    if q_ratio > 80:
+        print(f'Test passed with the score of QRatio of {q_ratio}!')
+    elif 60 < q_ratio < 80:
+        print(f'QRatio test passed with a score of {q_ratio}!')
+    else:
+        print(f'Test failed with a QRatio of {q_ratio}!')
+    return q_ratio
 
 # TODO QRatio from fuzzywuzzy
